9bbacb95d9b727323c99a89fab850e80b1d672aa
Tomas <tomas.huzevka@outlook.com>
2020-01-19 20:43:19 -0500

    homework

--- function_intermediate_II.py
+++ function_intermediate_II.py
@@ -1,4 +1,4 @@
-# 1. Update Values in Dictionaries and Lists   
+# Update Values in Dictionaries and Lists   
 
 x = [ [5,2,3], [10,8,9] ] 
 students = [
@@ -11,17 +11,39 @@ sports_directory = {
 }
 z = [ {'x': 10, 'y': 20} ]
 
-#     Change the value 10 in x to 15. Once you're done, x should now be [ [5,2,3], [15,8,9] ].
+# 1.     Change the value 10 in x to 15. Once you're done, x should now be [ [5,2,3], [15,8,9] ].
 x[1][0] = 15
 
-#     Change the last_name of the first student from 'Jordan' to 'Bryant'
+# 2.   Change the last_name of the first student from 'Jordan' to 'Bryant'
 students[0]['first_name'] = 'Bryant'
 
-#     In the sports_directory, change 'Messi' to 'Andres'
+# 3.   In the sports_directory, change 'Messi' to 'Andres'
 sports_directory['soccer'][0] = 'Andres'
 
-#     Change the value 20 in z to 30
+# 4.   Change the value 20 in z to 30
 z[0]['y'] = 30
+
+# 5. Iterate Through a List of Dictionaries
+# Create a function iterateDictionary(some_list) that, given a list of dictionaries, the function loops through each dictionary in the list and prints each key and the associated value. For example, given the following list:
+students = [
+         {'first_name':  'Michael', 'last_name' : 'Jordan'},
+         {'first_name' : 'John', 'last_name' : 'Rosales'},
+         {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+         {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+
+def iterateDictionary(lst):
+    for i in range(len(lst)):
+        print(students[i])
+
+# 6. Create a function iterateDictionary2(key_name, some_list) that, given a list of dictionaries and a key name, the function prints the value stored in that key for each dictionary. For example, iterateDictionary2('first_name', students) should output:
+
+def iterateDictionary2(key_name, some_list):    
+    for i in range(len(some_list)):
+        print(students[key_name])
+
+# iterateDictionary2('first_name', students)
+print(students[0])
 
 
 
